f9d8ea632c047d994d4045ce85d81621c3075eda
Aristo <dkfud2121@naver.com>
2022-07-17 02:14:43 +0900

    feat : 임시 데이터셋 구축

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,27 @@
 from typing import Union
+from fastapi import FastAPI, APIRouter, Query, HTTPException  # 1
 
-from fastapi import FastAPI
+# 1 딕셔너리 형태로 예시 데이터 생성
+from backend.app.schemas import User
 
-app = FastAPI()
+USERS = [
+    {
+        "id": 1,
+        "name": "장아령",
+        "password": "WKDDKFUD",
+        "active": "TRUE",
+    },
+    {
+        "id": 2,
+        "name": "길연쨩",
+        "password": "RLFDUSwKD",
+        "active": "FALSE",
+    },
+]
 
+app = FastAPI(title='gz-salon')
+# api 통신을 위한 라우터 객체 생성
+api_router = APIRouter()
 
 @app.get("/")
 def read_root():
@@ -13,3 +31,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@api_router.get("/user/user_id", status_code=200, response_model=User)
+
+
+
