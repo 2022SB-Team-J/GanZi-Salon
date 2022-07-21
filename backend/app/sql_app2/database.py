4286c6c38e360e82d7918da3356e1ccf2aa8779b
김우현 <gim-uhyeon@gim-uhyeon-MacBook-Air.local>
2022-07-21 23:41:43 +0900

    날아간거다시 쓰는 중

--- backend/app/sql_app2/database.py
+++ backend/app/sql_app2/database.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_FILE = os.path.join(BASE_DIR,'secrets.json')
+secrets = json.loads(open(SECRET_FILE).read())
+DB = secrets["DB"]
 
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@
 #*_URL = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DBNAME}'
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:administrator@localhost:3306/testdb2"
 
