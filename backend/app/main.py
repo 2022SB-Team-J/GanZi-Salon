76ea623568834e7961ca749df77560243130d6c3
Aristo <dkfud2121@naver.com>
2022-07-21 11:14:27 +0900

    feat : front에 form 태그 형태로 서버 전송

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,18 +5,15 @@ from fastapi.security import OAuth2PasswordBearer
 # 1 딕셔너리 형태로 예시 데이터 생성
 # from .models import User
 
-from .routers import api_join, api_login
-# from .dependencies import get_query_token, get_token_header
-
+from .routers import users
+from .dependencies import get_query_token, get_token_header
 
 
 app = FastAPI(title='gz-salon')
+
 # app.include_router(register.api_router)
 app.include_router(api_login.api_router)
 app.include_router(api_join.api_router)
-
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -6,51 +6,78 @@ import { Link } from "react-router-dom";
 import Button from "../components/LoginButton";
 import Input from "../components/IdPassword";
 
+//백엔드와 프론트 통신 객체
+import axios from "axios"
+
+
 const Container = styled.div`
   margin-top: 100px;
   padding: 20px;
   text-align: center;
 `;
 
-function Login() {
+function SignUp() {
+
+//    회원가입 버튼 클릭 시 동작
+//    참고링크 >> https://velog.io/@dev_bomdong/React-회원가입-기능-구현하기
+    const handleSubmit = () => {
+        e.preventDefault();
+        try {
+            fetch('API주소', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username: this.state.username,
+                    id: this.state.id,
+                    password: this.state.password,
+                    gender: 'w'
+                }),
+            })
+                .then(response => response.json());
+        } catch (err) {
+            alert('회원가입 실패입니다.');
+        }
+    };
+
+
 return (
     
     <Container className="background-image-black" >
       
-    <div style = {{marginTop : '250px', marginBottom: '30px'}}>
-    <div className = "title-text-white"  >GANZI SALON</div>
+    <form style = {{marginTop : '250px', marginBottom: '30px'}}
+            onSubmit={handleSubmit}>
+    <form className = "title-text-white"  >GANZI SALON</form>
 
-    <div>
-    <Input 
-    id="id" 
-    name="id" 
-    placeholder="ID" 
-    />
-    </div>
-    <div>
-    <Input
-    id="password"
-    name="password"
-    type="password"
-    placeholder="PASSWORD"
-    />
-    </div>
-    <div>
-    <Input
-    id="username"
-    name="username"
-    type="username"
-    placeholder="NAME"
-    />
-    </div>
+        <div>
+        <Input
+        id="id"
+        name="id"
+        placeholder="ID"
+        />
+        </div>
+        <div>
+        <Input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="PASSWORD"
+        />
+        </div>
+        <div>
+        <Input
+        id="username"
+        name="username"
+        type="username"
+        placeholder="NAME"
+        />
+        </div>
       <div style = {{textAlign : 'center'}}>
       {/* 임시로 Title 페이지로 이동하게 세팅 */}
       
-      <Link to="/" style={{ textDecoration: "none" }}>
-      <Button signup2>Sign up</Button>
-      </Link> 
+          <Link to="/" style={{ textDecoration: "none" }}>
+          <Button signup2>Sign up</Button>
+          </Link>
       </div>
-      </div>
+      </form>
     </Container>
   );
 }
