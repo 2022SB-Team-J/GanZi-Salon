a97ef59369fd1f7841db8c158ebf5ab036dc8422
김우현 <gim-uhyeon@gim-uhyeon-MacBook-Air.local>
2022-07-24 23:11:51 +0900

    fix : 문제 해결
    기존 자잘한 문제 해결

--- backend/app/database.py
+++ backend/app/database.py
@@ -15,7 +15,7 @@ SECRET_FILE = os.path.join(BASE_DIR,'secrets.json')
 secrets = json.loads(open(SECRET_FILE).read())
 DB = secrets["DB"]
 
-DB_URL = f"mysql+pymysql//{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
 
 engine = create_engine(
     DB_URL , encoding = 'utf-8'
--- backend/app/models.py
+++ backend/app/models.py
@@ -4,32 +4,30 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 #import needed one
 from .database import Base
-#from datetime import datetime
+from datetime import  datetime
 # . means local
 
 class User(Base):
     #user table setup
     __tablename__ = "Users"
 
-    user_id = Column(String(20), primary_key=True, index=True)
+    user_id = Column(String(20), primary_key=True)
     name = Column(String(20), unique=True, index=True)
-    password = Column(String(100) )
-    #create_at = Column(String(30), default = datetime.datetime.now() )
-    #upload_at = Column(String(30), default = datetime.datetime.now() )
+    password = Column(String(100) ,index = True )
+    create_at = Column(String(30) ,index = True, default = datetime.now())
+    upload_at = Column(String(30) ,index = True, default = datetime.now())
 
-    active = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=True,index = True)
     #gender = Column(Boolean, default = True) #가능성 유
-    gender = Column(String(1), default = 'N' )
-
-    images = relationship("Image", back_populates="owner")
+    gender = Column(String(1), default = 'N',index = True )
 
 
     #image table setup
 class Image(Base):
     __tablename__ = "Images"
 
-    autonum = Column(Integer, primary_key=True,  autonum = True, index = True) #you can see user : item is constructed 1 : N forms
-    user_id = Column(String(20), ForeignKey('Users', index=True))
+    autonum = Column(Integer, primary_key=True) #you can see user : item is constructed 1 : N forms
+    user_id = Column(String(20), ForeignKey('Users'),  index=True )
     image_url = Column(String(100), index = True)
-   # create_at = Column(String(30), datetime.datetime.now(),index = True )
+    create_at = Column(String(30),index = True ,default = datetime.now() )
 #우선 datetime 을 저장하기위해 String 형태로 저장하게 하였으나, 더 나은 방법이있다면 언급부탁드립니다.
