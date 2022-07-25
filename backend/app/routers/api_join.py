31bd400c910e4560619b06c44c56ac5aa718aa2e
Aristo <dkfud2121@naver.com>
2022-07-25 17:52:43 +0900

    refactor : 프론트에서 임시로 데이터 받는 객체 생성

--- backend/app/routers/api_join.py
+++ backend/app/routers/api_join.py
@@ -13,34 +13,38 @@ api_router = APIRouter(
     # dependencies=[Depends(get_query_token)]
 )
 
+@api_router.post("/auth/user/join")
+async def join(user:User):
+    print(user)
+
 # https://github.com/tiangolo/full-stack-fastapi-postgresql/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/backend/app/app/crud/crud_user.py
-# 허밍버드 >>https://github.com/kpuce2022CD/hummingbird/blob/06f5a1a620b398db70fd9fb9d18900718a641b66/frontend/components/SignUpPage/SignUpForm/SignUpForm.tsx4
-# https://github.com/RowinChalas/IonicFastHospital/blob/c49a149875c2f2b723913bdc15a84e3da7ed66cc/src/app/pages/inicio-sesion/inicio-sesion.page.ts
-# https://github.com/zusdn90/fastapi-signUp-API/blob/b3a161201c18e22a5bc8b2a46d7917211faff1a0/backend/app/api/v1/register.py
-@api_router.post("/register_user", status_code=200)
-async def register(reg_info:JoinUser):
-    # 회원가입
-    if reg_info.id :
-        return JSONResponse(status_code=400, content=dict(msg="이미 존재하는 ID 입니다."))
-    if not reg_info.id or reg_info.password:
-        return JSONResponse(status_code=400, content=dict(msg="ID and Password must be provided"))
-    if not reg_info.password == reg_info.check_password:
-        return JSONResponse(status_code=400, content=dict(msg="비밀번호를 올바르게 썼는지 확인해주세요."))
-    if not reg_info.gender :
-        return JSONResponse(status_code=400, content=dict("성별을 입력해주세요"))
-
-    # 비밀번호 암호화
-    # 여기 말고 crud 할 때 암호화 해도 될 듯?
-    # https://github.com/tiangolo/full-stack-fastapi-postgresql/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/backend/app/app/crud/crud_user.py
-    hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt())
-
-    # user = crud.user.create(db, obj_in=user_in)
-    # crud에 들어갈 내용 대체
-    new_user = User(
-        id = reg_info.id,
-        name = reg_info.name,
-        gender = reg_info.gender,
-        password = hash_pw,
-        active = True
-    )
-    return new_user
+# # 허밍버드 >>https://github.com/kpuce2022CD/hummingbird/blob/06f5a1a620b398db70fd9fb9d18900718a641b66/frontend/components/SignUpPage/SignUpForm/SignUpForm.tsx4
+# # https://github.com/RowinChalas/IonicFastHospital/blob/c49a149875c2f2b723913bdc15a84e3da7ed66cc/src/app/pages/inicio-sesion/inicio-sesion.page.ts
+# # https://github.com/zusdn90/fastapi-signUp-API/blob/b3a161201c18e22a5bc8b2a46d7917211faff1a0/backend/app/api/v1/register.py
+# @api_router.post("/register_user", status_code=200)
+# async def register(reg_info:JoinUser):
+#     # 회원가입
+#     if reg_info.id :
+#         return JSONResponse(status_code=400, content=dict(msg="이미 존재하는 ID 입니다."))
+#     if not reg_info.id or reg_info.password:
+#         return JSONResponse(status_code=400, content=dict(msg="ID and Password must be provided"))
+#     if not reg_info.password == reg_info.check_password:
+#         return JSONResponse(status_code=400, content=dict(msg="비밀번호를 올바르게 썼는지 확인해주세요."))
+#     if not reg_info.gender :
+#         return JSONResponse(status_code=400, content=dict("성별을 입력해주세요"))
+#
+#     # 비밀번호 암호화
+#     # 여기 말고 crud 할 때 암호화 해도 될 듯?
+#     # https://github.com/tiangolo/full-stack-fastapi-postgresql/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/backend/app/app/crud/crud_user.py
+#     hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt())
+#
+#     # user = crud.user.create(db, obj_in=user_in)
+#     # crud에 들어갈 내용 대체
+#     new_user = User(
+#         id = reg_info.id,
+#         name = reg_info.name,
+#         gender = reg_info.gender,
+#         password = hash_pw,
+#         active = True
+#     )
+#     return new_user
--- backend/app/routers/api_login.py
+++ backend/app/routers/api_login.py
@@ -10,19 +10,23 @@ api_router = APIRouter(
     # dependencies=[Depends(get_query_token)]
 )
 
-@api_router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+@api_router.post("/auth/user/login")
+async def login(user:User):
+    print(user)
 
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@api_router.get("/users/me")
-async def read_users_me(current_user: User = Depends(auth.get_current_active_user)):
-    return current_user
+# @api_router.post("/token")
+# async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+#     user_dict = fake_users_db.get(form_data.username)
+#     if not user_dict:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#     user = UserInDB(**user_dict)
+#     hashed_password = fake_hash_password(form_data.password)
+#     if not hashed_password == user.hashed_password:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#
+#     return {"access_token": user.username, "token_type": "bearer"}
+#
+#
+# @api_router.get("/users/me")
+# async def read_users_me(current_user: User = Depends(auth.get_current_active_user)):
+#     return current_user
