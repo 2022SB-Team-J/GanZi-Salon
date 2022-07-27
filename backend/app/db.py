88a1feb8dab038cbd3513e4c53d6da0df0849eca
gilyeon00 <12venus08@naver.com>
2022-07-27 15:52:11 +0900

    fix : setting docker env

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,27 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-import os
 from pathlib import Path
 from dotenv import load_dotenv
+import json
+import os
 
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# ENV_DIR = BASE_DIR.parent.parent
-# print(ENV_DIR)
-# load_dotenv(os.path.join(ENV_DIR, ".env"))
+# #ENV_DIR = (BASE_DIR.parent).parent
+# #ENV_DIR = Path(__file__).resolve(strict=True).parent
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # user_name = os.environ["DB_USER"]
 # password = os.environ["DB_ROOT_PASSWORD"]
 # database_name = os.environ["DB_DATABASE"]
-# host = "localhost"
+# host = os.environ["DB_HOST"]
 
-user_name = "root"
-password = "0701as"
-database = "gzsalon"
-host = "localhost"
-
-
+# DATABASE = f"mysql+pymysql://{user_name}:{password}@{host}:3306/{database_name}?charset=utf8" 
 DATABASE = f"mysql+pymysql://root:0701as@db:3306/gzsalon?charset=utf8" 
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# SECRET_FILE = os.path.join(BASE_DIR,'secrets.json')
+# secrets = json.loads(open(SECRET_FILE).read())
+# DB = secrets["DB"]
+
+# DATABASE = f"mysql+pymysql://{DB['user']}:{DB['password']}@db:3306/{DB['database']}?charset=utf8"
+
 
 ENGINE = create_engine(
     DATABASE,
