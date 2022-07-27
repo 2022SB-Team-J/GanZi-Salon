583890d263c9b04975f1d6a1cc32eb9f31e08244
Aristo <dkfud2121@naver.com>
2022-07-27 16:39:20 +0900

    refactor : 회원가입 api 추가

--- backend/app/auth/schemas.py
+++ backend/app/auth/schemas.py
@@ -2,15 +2,16 @@
 # typing ?  https://www.daleseo.com/python-typing/
 from datetime import datetime
 from typing import Sequence
+from fastapi import HTTPException
 
 import bcrypt
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from starlette import status
+
 from backend.app import model
 
 
-def get_all(db: Session):
-    return db.query(model.UserTable).all()
 
 # 유저
 class User(BaseModel):
@@ -26,12 +27,12 @@ class User(BaseModel):
     def __init__(self, user_id,username, password, gender):
         self.user_id=user_id
         self.username=username
-        self.hashed_password=HashPwd(password)
+        self.hashed_password=password
         self.gender=gender
         self.active = True
 
     # 업데이트. 업데이트 일자 추가 예정
-    def updeate(self):
+    def updeate(self, user_id, username, password, update_at):
         pass
     # 비활성화. 업데이트 일자 추가 예정
     def inactive(self):
@@ -44,17 +45,35 @@ class HashPwd(BaseModel):
         self.hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
+#회원가입
+def createUser(request:User, db:Session):
+    hashed_password = HashPwd(request.password)
+    user = User( request.user_id, request.username, hashed_password, request.gender)
+    db.add(user)
+    db.commit()
+    db.refresh()
+    return user
+
+# 회원 조회
+def showUser(id: int, db: Session):
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found"
+        )
+    return user
+
+def get_all(db: Session):
+    return db.query(model.UserTable).all()
 
 
 
 
-
-
-# 3  uses Pydantic’s recursive capability to define a field
-#   that refers to another Pydantic class we’ve previously defined, the Recipe class
-class UserSearchResults(BaseModel):
-    # 4 Sequence (which is an iterable with support for len and __getitem__) of Recipes.
-    results: Sequence[User]
-
-class Token(BaseModel):
-    Authorization: str = None
+# # 3  uses Pydantic’s recursive capability to define a field
+# #   that refers to another Pydantic class we’ve previously defined, the Recipe class
+# class UserSearchResults(BaseModel):
+#     # 4 Sequence (which is an iterable with support for len and __getitem__) of Recipes.
+#     results: Sequence[User]
+#
+# class Token(BaseModel):
+#     Authorization: str = None
--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -1,11 +1,10 @@
 import bcrypt
 from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 api_router = APIRouter(
     tags=["register"],
     # dependencies=[Depends(get_query_token)]
 )
-
-# 회원가입
