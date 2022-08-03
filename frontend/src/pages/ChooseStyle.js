9b0089e19b820188c14e6cd11fd2dfb5b2bc6d48
wisdomis <108566232+wisdomis@users.noreply.github.com>
2022-08-04 02:22:34 +0900

    chore : 위치, 텍스트 추가

--- frontend/src/pages/ChooseStyle.js
+++ frontend/src/pages/ChooseStyle.js
@@ -9,11 +9,14 @@ return (
     <div className="background-image-main">
 
     <div className = "title-text-black-3">Choose hair style or upload</div>
-    <div style = {{width: '100%', textAlign:'center', fontSize: '30px', fontFamily:'ariblk', paddingTop: '20px', color : '#9370db', fontWeight:'bold'}}>
+    <div style = {{marginLeft: '100px',  fontSize: '30px', fontFamily:'ariblk', paddingTop: '20px', color : '#9370db', fontWeight:'bold'}}>
     Female
     </div>
     <FemaleSlide />
-    <div style = {{width: '100%', textAlign:'center', fontSize: '30px', fontFamily:'ariblk', paddingTop: '20px', color : '#4682b4', fontWeight:'bold'}}>
+    <div style = {{textAlign: 'center',  fontSize: '30px', fontFamily:'ariblk', paddingTop: '20px', color : 'Gray', fontWeight:'bold'}}>
+    Please push to the side and select a picture.
+    </div>
+    <div style = {{marginLeft: '120px', fontSize: '30px', fontFamily:'ariblk', paddingTop: '20px', color : '#4682b4', fontWeight:'bold'}}>
     Male
      </div>
     <MaleSlide />
